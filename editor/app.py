2bb443da891e02186ca86d633f0cb9af3eadd03f
brianhe12 <brian.he17@gmail.com>
2020-04-17 16:41:25 -0400

    added database

--- editor/app.py
+++ editor/app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -9,8 +14,16 @@ def quill():
 
 @app.route('/process', methods=['POST'])
 def get_post_json():
+    # Connect with Mongodb Atlas.
+    client = MongoClient(os.getenv('MONGO_STRING'))
+    db=client.p2p_docs
+
     data = request.get_json()
     print(data)
+
+    # Insert into database
+    db.documents.insert_one(data)
+
     return jsonify(status="success", data=data)
 
 
