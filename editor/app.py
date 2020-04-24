3ac12b36fbea08bd6c83e27acf826a074291ba4a
Danny Trinh <djtrinh@bu.edu>
2020-04-24 15:27:11 -0400

    Added webpack for YJS Quill, added yjs example, remove unneeded node_modules folder, integrated front end code. WARNING:backend integration in progress

--- editor/app.py
+++ editor/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for, make_response
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
@@ -7,9 +7,21 @@ load_dotenv()
 
 app = Flask(__name__)
 
+number_of_users = 2
 
 @app.route('/', methods=['GET'])
-def quill():
+def home():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def keyCheck():
+    key1 = request.form['key1']  # getting usernames
+
+    if key1 == "password":
+        return redirect('http://127.0.0.1:8080/quill.html', code=301)
+
+@app.route('/editor')
+def editor():
     return render_template('quill.html')
 
 @app.route('/process', methods=['POST'])
