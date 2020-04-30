73b6f234fc9bc80acb58a6216e08c8f697620a1f
Edwin Sun <34005715+edsun123@users.noreply.github.com>
2020-04-30 17:58:11 -0400

    commented out the wrong syntax

--- editor/test_enc_data.py
+++ editor/test_enc_data.py
@@ -12,27 +12,26 @@ print("POSTING JSON but we are not retrieving information")
 client = MongoClient(os.getenv('MONGO_STRING'))
 db=client.p2p_docs
 
+#did not insert into the database yet...
 print("setting data as 'blah' 'blah'")
 #    data = request.get_json()
 data = "blah blah"
 
-#place holder for save
-
-#actual encryption when you save a document
-
+#creating file and user objects
 print("creating file 1 and user1")
 myfile="file1"
 user1=User("user1")
 user1.generate_userkeys()
 file1=File(user1, myfile)
 
+#creating cipher, userkey, and filekeys
 ---------------------------------------------------
 print("creating cipher, userkeys, and filekeys ")
 file1.generate_filekey()
 file1.cipher_gen()
 user1.generate_userkeys()
 
-
+#encrypting the data
 ----------------------------------------------------
 print("encrypting" + data)
 enc_data = file1.encrypt_data(data)
@@ -40,9 +39,8 @@ print("this is the encrypted data" + str(enc_data, 'utf-8'))
 #print("putting encrypted data into database")
 #db.documents.insert_one(enc_data)
 
-
+#decrypting the data
 ----------------------------------------------------
-#actual decryption when you are loading a document
 print("decrypting")
 print("generating encryption key")
 ek = encrypt_key(file1, user1)
