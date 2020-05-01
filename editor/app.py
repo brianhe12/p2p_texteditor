92b1b137cf962beba6becf8663a8a3460c341862
Danny Trinh <djtrinh@bu.edu>
2020-05-01 00:20:20 -0400

    pushing encode decode

--- editor/app.py
+++ editor/app.py
@@ -51,58 +51,58 @@ def editor():
     return render_template('quill.html')
 
 
-#may leave alone
-@app.route('/process', methods=['POST'])
-def get_post_json():
-    print("posting json onto database")
-    # Connect with Mongodb Atlas.
-    client = MongoClient(os.getenv('MONGO_STRING'))
-    db = client.p2p_docs
+# #may leave alone
+# @app.route('/process', methods=['POST'])
+# def get_post_json():
+#     print("posting json onto database")
+#     # Connect with Mongodb Atlas.
+#     client = MongoClient(os.getenv('MONGO_STRING'))
+#     db = client.p2p_docs
+#
+#     data = request.get_json()
+#     print(data)
+#
+#     ## in case if we don't use flask, we might need to move all operations to javascript
+#     user1 = User("user1")
+#     user1.generate_userkeys()
+#
+#     file1 = File(user1, myfile)
+#     file1.generate_filekey()
+#     file1.cipher_gen()
+#     enc_data = file1.encrypt_data(data)
+#
+#     # Insert into database
+#     # db.documents.insert_one(data)
+#     db.documents.insert_one(enc_data)
+#
+#     return jsonify(status="success", data=data)
 
-    data = request.get_json()
-    print(data)
 
-    ## in case if we don't use flask, we might need to move all operations to javascript
-    user1 = User("user1")
-    user1.generate_userkeys()
-
-    file1 = File(user1, myfile)
-    file1.generate_filekey()
-    file1.cipher_gen()
-    enc_data = file1.encrypt_data(data)
-
-    # Insert into database
-    # db.documents.insert_one(data)
-    db.documents.insert_one(enc_data)
-
-    return jsonify(status="success", data=data)
-
-
-@app.route('/getData')
-def get_data():
-    print("retrieving data from database")
-    # Connect with Mongodb Atlas.
-    client = MongoClient(os.getenv('MONGO_STRING'))
-    db = client.p2p_docs
-    collection = db['documents']
-
-    ek = encrypt_key(file1, user1)
-    decrypt_data(ek, user1, file1, enc_data)
-
-    cursor = collection.find({})
-    #    for document in cursor:
-    #        return str(document["ops"])
-    
-
-    for document in cursor:
-    
-        print("decrypting")
-        print("generating encryption key")
-        ek = encrypt_key(file1, user1)
-        dec_content = decrypt_data(ek, user1, file1, document['data'])
-        print ("this is decrypted data: "+str(dec_data, 'utf-8'))
-#        dec_content = decrypt_data(ek, user1, file1, str(document["ops"]))
-        return dec_content
+# @app.route('/getData')
+# def get_data():
+#     print("retrieving data from database")
+#     # Connect with Mongodb Atlas.
+#     client = MongoClient(os.getenv('MONGO_STRING'))
+#     db = client.p2p_docs
+#     collection = db['documents']
+#
+#     ek = encrypt_key(file1, user1)
+#     decrypt_data(ek, user1, file1, enc_data)
+#
+#     cursor = collection.find({})
+#     #    for document in cursor:
+#     #        return str(document["ops"])
+#
+#
+#     for document in cursor:
+#
+#         print("decrypting")
+#         print("generating encryption key")
+#         ek = encrypt_key(file1, user1)
+#         dec_content = decrypt_data(ek, user1, file1, document['data'])
+#         print ("this is decrypted data: "+str(dec_data, 'utf-8'))
+# #        dec_content = decrypt_data(ek, user1, file1, str(document["ops"]))
+#         return dec_content
 
 @app.route('/test', methods=['POST'])
 def test():
@@ -111,11 +111,9 @@ def test():
 
     data = request.get_json()
     print(data)
-    
-    data = json.dumps(data)
 
     # Encrypt
-    encrypted_message = encryption_type.encrypt(data.encode())
+    encrypted_message = encryption_type.encrypt(json.dumps(data).encode())
     print(encrypted_message)
 
     # Write to file
@@ -130,14 +128,14 @@ def test():
 def testGet():
     global key
     global encryption_type
-
     # Read from file and decrypt
     with open('database.txt', "rb") as f:
-        encrypted_message = (f.readline())
-        print(encrypted_message)
-        
-        # decrypted_message = encryption_type.decrypt(encrypted_message)
-        # print(decrypted_message)
+        encrypted_message = f.readline()
+        print(encrypted_message.decode('utf-8'))
+        global key
+        global encryption_type
+        decrypted_message = encryption_type.decrypt(encrypted_message)
+        print(decrypted_message)
 
     return {'data': 'test data'}
 
